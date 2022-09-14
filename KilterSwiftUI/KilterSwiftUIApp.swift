f420ac39b6ceef09509355904e2cb5089c44ed5b
nikitanesporov <nikitanesporov@gmail.com>
2022-09-14 23:55:28 +0500

    Initial commit

---
+++ KilterSwiftUI/KilterSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  KilterSwiftUIApp.swift
+//  KilterSwiftUI
+//
+//  Created by Nikita Nesporov on 14.09.2022.
+//
+
+import SwiftUI
+
+@main
+struct KilterSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
